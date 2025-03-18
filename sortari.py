f8cc2e014a40a4a61f56f2b9a23f26d2e9b74731
21vidaloca <apostoldavid21@gmail.com>
2025-03-18 17:20:11 +0200

    second commit

--- sortari.py
+++ sortari.py
@@ -1,5 +1,9 @@
-v=[7,8,5643,123,32,7.6,7.59,13,5555,99898,3,5,76]
+v=[7,8,5643,123, 123, 32,32,7.6,7.59,13,5555,99898,3,5,76]
+n=len(v)
+maxx=max(v)
 from merge_sort import merge_sort
-print(len(v))
-merge_sort(0,len(v)-1,v)
+from count_sort import count_sort
+# print(len(v))
+# merge_sort(0,len(v)-1,v)
+count_sort(v,maxx)
 print(v)
