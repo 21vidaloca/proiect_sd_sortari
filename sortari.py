b272774e31ec667fca54ccc33ae8569c970f3a46
21vidaloca <apostoldavid21@gmail.com>
2025-03-31 14:05:35 +0300

    update - am facut heap sort

--- sortari.py
+++ sortari.py
@@ -2,8 +2,8 @@ v=[7,8,5643,123, 123, 32,32,7.6,7.59,13,5555,99898,3,5,76]
 n=len(v)
 maxx=max(v)
 from merge_sort import merge_sort
-from count_sort import count_sort
+from heap_sort import heapsort
 # print(len(v))
 # merge_sort(0,len(v)-1,v)
-count_sort(v,maxx)
+heapsort(v)
 print(v)
